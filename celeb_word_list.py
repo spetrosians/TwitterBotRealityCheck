a88abde06829a2c3a652aa6ef8f086bae3e31f4d
spetrosians <sevana.petrosians.555@my.csun.edu>
2014-11-19 21:52:46 -0800

    celeb word list
    added the celeb top word list. Minor fixtures.

--- celeb_word_list.py
+++ celeb_word_list.py
@@ -1,1 +1,13 @@
-#paste the disctionary
+#paste the dictionary
+
+
+celeb_word_list={'rihanna': ['love', 'like', 'chris', 'single', 'bravo', 'album', 'girl', 'music', 'song', 'eminem', 'amas', 'life', 'dinner', 'fan', 'people'],
+ 'lady gaga': ['love', 'monster', 'inspiration', 'million', 'wild', 'endlessly', 'blessings', 'purpose', 'special', 'like', 'bravery', 'veterens', 'best', 'amazing', 'music'],
+ 'kim kardashian': ['famous', 'hollywoood', 'celebrity', 'rayj', 'kanyewest', 'khloe', 'coffee', 'kourtney', 'hamptons', 'champagne', 'glass', 'papermagazine', 'love', 'breaktheinternet', 'wear'],
+ 'miley cyrus': ['love', 'like', 'smile', 'rainbow', 'freaks', 'stop', 'music', 'hannah', 'best', 'twerked', 'wrecking', 'bangerz', 'pop', 'life', 'show'], 
+ 'taylor swift': ['love', 'album', 'like', 'music', 'excited', 'friends', 'always', 'shake', 'crowns', 'never', 'wrong', 'proud', 'beaches', 'amazing', 'rest'],
+ 'nicki minaj': ['video', 'bang', 'love', 'song', 'like', 'anaconda', 'drake', 'arianagrande', 'jessiej', 'booty', 'world', 'pokemon', 'life', 'music', 'duck'],
+ 'katy perry': ['favorite', 'love', 'like', 'wear', 'artist', 'amas', 'birthday', 'single', 'heaven', 'earth', 'female', 'girl', 'life', 'firework', 'baby'],
+ 'selena gomez': ['thought', 'love', 'like', 'believe', 'friends', 'lucky', 'listen', 'voice', 'night', 'beautiful', 'night', 'justinbieber', 'inspiring', 'really', 'song'],
+ 'shakira': ['hips', 'murder', 'night', 'celebrate', 'hot', 'milestone', 'joy', 'lie', 'best', 'music', 'fall', 'waka', 'life', 'loved', 'stay'],
+ 'oprah winfrey': ['foot', 'gracious', 'wise', 'trust', 'like', 'queen', 'life', 'love', 'privilege', 'luck', 'believe', 'show', 'network', 'glamours', 'attitude']}
