a7b326f8230e5299ea15a697c13d173e83c8e2df
rcholvin <r_cholvin@yahoo.com>
2014-11-20 16:03:08 -0800

    added names to the words dictionary

--- celeb_word_list.py
+++ celeb_word_list.py
@@ -1,7 +1,8 @@
 #paste the dictionary
 
 
-celeb_word_list={'rihanna': ['love', 'like', 'chris', 'single', 'bravo', 'album', 'girl', 'music', 'song', 'eminem', 'amas', 'life', 'dinner', 'fan', 'people'],
+celeb_word_list={
+ 'rihanna': ['love', 'like', 'chris', 'single', 'bravo', 'album', 'girl', 'music', 'song', 'eminem', 'amas', 'life', 'dinner', 'fan', 'people'],
  'lady gaga': ['love', 'monster', 'inspiration', 'million', 'wild', 'endlessly', 'blessings', 'purpose', 'special', 'like', 'bravery', 'veterens', 'best', 'amazing', 'music'],
  'kim kardashian': ['famous', 'hollywoood', 'celebrity', 'rayj', 'kanyewest', 'khloe', 'coffee', 'kourtney', 'hamptons', 'champagne', 'glass', 'papermagazine', 'love', 'breaktheinternet', 'wear'],
  'miley cyrus': ['love', 'like', 'smile', 'rainbow', 'freaks', 'stop', 'music', 'hannah', 'best', 'twerked', 'wrecking', 'bangerz', 'pop', 'life', 'show'], 
@@ -10,4 +11,11 @@ celeb_word_list={'rihanna': ['love', 'like', 'chris', 'single', 'bravo', 'album'
  'katy perry': ['favorite', 'love', 'like', 'wear', 'artist', 'amas', 'birthday', 'single', 'heaven', 'earth', 'female', 'girl', 'life', 'firework', 'baby'],
  'selena gomez': ['thought', 'love', 'like', 'believe', 'friends', 'lucky', 'listen', 'voice', 'night', 'beautiful', 'night', 'justinbieber', 'inspiring', 'really', 'song'],
  'shakira': ['hips', 'murder', 'night', 'celebrate', 'hot', 'milestone', 'joy', 'lie', 'best', 'music', 'fall', 'waka', 'life', 'loved', 'stay'],
- 'oprah winfrey': ['foot', 'gracious', 'wise', 'trust', 'like', 'queen', 'life', 'love', 'privilege', 'luck', 'believe', 'show', 'network', 'glamours', 'attitude']}
+ 'oprah winfrey': ['foot', 'gracious', 'wise', 'trust', 'like', 'queen', 'life', 'love', 'privilege', 'luck', 'believe', 'show', 'network', 'glamours', 'attitude']
+ 'ashton kutcher': ['alia', 'film', 'fan', 'bhatt', 'hollywood', 'short', 'found', 'mila', 'kunis', 'safety', 'praised', 'half', 'star', 'baby', 'actor', 'hunk'],
+ 'britney spears': ['love', 'follow', 'behind', 'turns', 'bonus', 'spirit', 'join', 'please', 'stage', 'gorgeous', 'night', 'told', 'great', 'happens', 'excited', 'little']
+ 'chris brown': ['like', 'girl', 'aint', 'said', 'cheetah', 'tamar', 'love', 'nigga', 'people', 'expect', 'adrienne', 'ass', 'real', 'bailon', 'karrueche', 'braxton']
+ 'jennifer lopez': ['su', 'la', 'figura', 'campaa', 'abdomen', 'luce', 'plano', 'cantante', 'protagoniza', 'presume', 'love', 'booty', 'million', 'day', 'chelsea', 'house']
+ 'justin bieber': ['love', 'please', 'follow', 'much', 'vote', 'beliebers', 'na', 'smile', 'know', 'hi', 'see', 'like', 'wan', 'life', 'one', 'codysimpson']
+ 'justin timberlake': ['man', 'beautiful', 'times', 'human', 'bad', 'thing', 'restaurantes', 'love', 'like', 'imagina', 'quer', 'nesses', 'pfvr', 'comer', 'dono', 'voice']
+ }
