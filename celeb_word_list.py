7d213441765db2b988e93b2d7b908276b9b9bfa3
spetrosians <sevana.petrosians.555@my.csun.edu>
2014-11-23 23:17:09 -0800

    Revert "minor changes"
    This reverts commit 172b347e0e7df0da85cf6ef9a2e390a2309d2160.

--- celeb_word_list.py
+++ celeb_word_list.py
@@ -1,71 +1,59 @@
+#paste the dictionary
+
 
 celeb_word_list={
  'rihanna': 
-<<<<<<< HEAD
             {'yes':['love', 'like', 'chris', 'single', 'bravo', 'album', 'girl', 'music', 'song', 'eminem', 'amas', 'life', 'dinner', 'fan', 'people'],
-              'no':['follow' , 'click', 'win'] },
-=======
-    {'yes':['love', 'like', 'chris', 'single', 'bravo', 'album', 'girl', 'music', 'song', 'eminem', 'amas', 'life', 'dinner', 'fan', 'people'],
-    'no':[] },
->>>>>>> origin/master
+              'no':[] },
  'lady gaga': 
     {'yes':['love', 'monster', 'inspiration', 'million', 'wild', 'endlessly', 'blessings', 'purpose', 'special', 'like', 'bravery', 'veterens', 'best', 'amazing', 'music'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'kim kardashian': 
     {'yes': ['famous', 'hollywoood', 'celebrity', 'rayj', 'kanyewest', 'khloe', 'coffee', 'kourtney', 'hamptons', 'champagne', 'glass', 'papermagazine', 'love', 'breaktheinternet', 'wear'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'miley cyrus': 
     {'yes': ['love', 'like', 'smile', 'rainbow', 'freaks', 'stop', 'music', 'hannah', 'best', 'twerked', 'wrecking', 'bangerz', 'pop', 'life', 'show'], 
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'taylor swift': 
      {'yes':['love', 'album', 'like', 'music', 'excited', 'friends', 'always', 'shake', 'crowns', 'never', 'wrong', 'proud', 'beaches', 'amazing', 'rest'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'nicki minaj':
     {'yes': ['video', 'bang', 'love', 'song', 'like', 'anaconda', 'drake', 'arianagrande', 'jessiej', 'booty', 'world', 'pokemon', 'life', 'music', 'duck'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'katy perry': 
      {'yes':['favorite', 'love', 'like', 'wear', 'artist', 'amas', 'birthday', 'single', 'heaven', 'earth', 'female', 'girl', 'life', 'firework', 'baby'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'selena gomez': 
      {'yes':['thought', 'love', 'like', 'believe', 'friends', 'lucky', 'listen', 'voice', 'night', 'beautiful', 'night', 'justinbieber', 'inspiring', 'really', 'song'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'shakira': 
     {'yes': ['hips', 'murder', 'night', 'celebrate', 'hot', 'milestone', 'joy', 'lie', 'best', 'music', 'fall', 'waka', 'life', 'loved', 'stay'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'oprah winfrey': 
      {'yes':['foot', 'gracious', 'wise', 'trust', 'like', 'queen', 'life', 'love', 'privilege', 'luck', 'believe', 'show', 'network', 'glamours', 'attitude'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'ashton kutcher': 
      {'yes':['alia', 'film', 'fan', 'bhatt', 'hollywood', 'short', 'found', 'mila', 'kunis', 'safety', 'praised', 'half', 'star', 'baby', 'actor', 'hunk'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'britney spears': 
     {'yes': ['love', 'follow', 'behind', 'turns', 'bonus', 'spirit', 'join', 'please', 'stage', 'gorgeous', 'night', 'told', 'great', 'happens', 'excited', 'little'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'chris brown':
      {'yes':['like', 'girl', 'aint', 'said', 'cheetah', 'tamar', 'love', 'nigga', 'people', 'expect', 'adrienne', 'ass', 'real', 'bailon', 'karrueche', 'braxton'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'jennifer lopez': 
      {'yes':['su', 'la', 'figura', 'campaa', 'abdomen', 'luce', 'plano', 'cantante', 'protagoniza', 'presume', 'love', 'booty', 'million', 'day', 'chelsea', 'house'],
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  'justin bieber': 
      {'yes':['love', 'much', 'vote', 'beliebers', 'na', 'smile', 'know', 'hi', 'see', 'like', 'wan', 'life', 'one', 'codysimpson'],
-    'no':['(x*[0-9]+)','follow', 'click', 'win'] },
+    'no':['(x*[0-9]+)','follow'] },
  'justin timberlake': 
     {'yes': ['man', 'beautiful', 'times', 'human', 'bad', 'thing', 'restaurantes', 'love', 'like', 'imagina', 'quer', 'nesses', 'pfvr', 'comer', 'dono', 'voice'],
-<<<<<<< HEAD
-    'no':['follow' , 'click', 'win'] },
+    'no':[] },
  }
+ 
  
 name_list=['ashton kutcher',
-=======
-    'no':[] },
- 'beyonce': {'yes':['artist', 'year', 'amas', 'album', 'love', 'new', 'like', 'people', 'want', 'tell', 'think', 'girl', 'band', 'secret', 'never', 'even'],
-    'no':[]}
- }
- 
- 
-name_list=[#'ashton kutcher',
->>>>>>> origin/master
 'beyonce',
 'britney spears',
 'chris brown',
