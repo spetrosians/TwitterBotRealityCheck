4cf7ca19717afd03d2045ae67d3ae0c76b4eb784
spetrosians <sevana.petrosians.555@my.csun.edu>
2014-11-30 02:53:25 -0800

    NO list
    added to the no list

--- celeb_word_list.py
+++ celeb_word_list.py
@@ -5,54 +5,54 @@
 celeb_word_list={
  'rihanna': 
     {'yes':['love', 'like', 'chris', 'single', 'bravo', 'album', 'girl', 'music', 'song', 'eminem', 'amas', 'life', 'dinner', 'fan', 'people'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'lady gaga': 
     {'yes':['love', 'monster', 'inspiration', 'million', 'wild', 'endlessly', 'blessings', 'purpose', 'special', 'like', 'bravery', 'veterens', 'best', 'amazing', 'music'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'kim kardashian': 
     {'yes': ['famous', 'hollywoood', 'celebrity', 'rayj', 'kanyewest', 'khloe', 'coffee', 'kourtney', 'hamptons', 'champagne', 'glass', 'papermagazine', 'love', 'breaktheinternet', 'wear'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'miley cyrus': 
     {'yes': ['love', 'like', 'smile', 'rainbow', 'freaks', 'stop', 'music', 'hannah', 'best', 'twerked', 'wrecking', 'bangerz', 'pop', 'life', 'show'], 
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'taylor swift': 
      {'yes':['love', 'album', 'like', 'music', 'excited', 'friends', 'always', 'shake', 'crowns', 'never', 'wrong', 'proud', 'beaches', 'amazing', 'rest'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'nicki minaj':
     {'yes': ['video', 'bang', 'love', 'song', 'like', 'anaconda', 'drake', 'arianagrande', 'jessiej', 'booty', 'world', 'pokemon', 'life', 'music', 'duck'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'katy perry': 
      {'yes':['favorite', 'love', 'like', 'wear', 'artist', 'amas', 'birthday', 'single', 'heaven', 'earth', 'female', 'girl', 'life', 'firework', 'baby'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'selena gomez': 
      {'yes':['thought', 'love', 'like', 'believe', 'friends', 'lucky', 'listen', 'voice', 'night', 'beautiful', 'night', 'justinbieber', 'inspiring', 'really', 'song'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'shakira': 
     {'yes': ['hips', 'murder', 'night', 'celebrate', 'hot', 'milestone', 'joy', 'lie', 'best', 'music', 'fall', 'waka', 'life', 'loved', 'stay'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'oprah winfrey': 
      {'yes':['foot', 'gracious', 'wise', 'trust', 'like', 'queen', 'life', 'love', 'privilege', 'luck', 'believe', 'show', 'network', 'glamours', 'attitude'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'ashton kutcher': 
      {'yes':['alia', 'film', 'fan', 'bhatt', 'hollywood', 'short', 'found', 'mila', 'kunis', 'safety', 'praised', 'half', 'star', 'baby', 'actor', 'hunk'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'britney spears': 
     {'yes': ['love', 'follow', 'behind', 'turns', 'bonus', 'spirit', 'join', 'please', 'stage', 'gorgeous', 'night', 'told', 'great', 'happens', 'excited', 'little'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'chris brown':
      {'yes':['like', 'girl', 'aint', 'said', 'cheetah', 'tamar', 'love', 'nigga', 'people', 'expect', 'adrienne', 'ass', 'real', 'bailon', 'karrueche', 'braxton'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'jennifer lopez': 
      {'yes':['su', 'la', 'figura', 'campaa', 'abdomen', 'luce', 'plano', 'cantante', 'protagoniza', 'presume', 'love', 'booty', 'million', 'day', 'chelsea', 'house'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'justin bieber': 
      {'yes':['love', 'much', 'vote', 'beliebers', 'na', 'smile', 'know', 'hi', 'see', 'like', 'wan', 'life', 'one', 'codysimpson'],
-    'no':['(x*[0-9]+)','follow'] },
+    'no':['(x*[0-9]+)','follow','click', 'win'] },
  'justin timberlake': 
     {'yes': ['man', 'beautiful', 'times', 'human', 'bad', 'thing', 'restaurantes', 'love', 'like', 'imagina', 'quer', 'nesses', 'pfvr', 'comer', 'dono', 'voice'],
-    'no':[] },
+    'no':['click', 'win', 'follow'] },
  'beyonce': {'yes':['artist', 'year', 'amas', 'album', 'love', 'new', 'like', 'people', 'want', 'tell', 'think', 'girl', 'band', 'secret', 'never', 'even'],
-    'no':[]}
+    'no':['click', 'win', 'follow']}
  }
  
 name_list=['ashton kutcher',
