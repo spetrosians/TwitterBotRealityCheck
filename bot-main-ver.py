531ce5429397f8abb852c96fba3f2ae238adde44
faina0005@gmail.com <faina0005@users.noreply.github.com>
2014-12-02 21:03:57 -0800

    oops

--- bot-main-ver.py
+++ bot-main-ver.py
@@ -586,8 +586,8 @@ if __name__ == "__main__":
                 else:                               #otherwise, get a respond list
                     search_lim=SEARCH_LIM 
                 
-                if len(id_list_str)==0:
-                    to_respond=id_list_str.keys()
+            if len(id_list_str)==0:
+                to_respond=id_list_str.keys()
 
             
             if  (datetime.utcnow()-break_int_mention).seconds>SLEEP_INT_MENT:
